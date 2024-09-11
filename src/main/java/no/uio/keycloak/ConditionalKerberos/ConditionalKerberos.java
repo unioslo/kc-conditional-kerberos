4288ceb8cf4a9a56d65638a458231cc12386258d
Francis Augusto Medeiros-Logeay <franciaa@usit.uio.no>
2024-09-11 16:35:19 +0200

    Working version

--- src/main/java/no/uio/keycloak/ConditionalKerberos/ConditionalKerberos.java
+++ src/main/java/no/uio/keycloak/ConditionalKerberos/ConditionalKerberos.java
@@ -3,23 +3,40 @@ package no.uio.keycloak.ConditionalKerberos;
 import org.jboss.logging.Logger;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.authenticators.browser.SpnegoAuthenticator;
+import org.keycloak.authentication.FlowStatus;
 import org.keycloak.sessions.AuthenticationSessionModel;
 import org.keycloak.models.AuthenticatorConfigModel;
 import org.keycloak.http.HttpRequest;
+import org.keycloak.http.HttpResponse;
 import org.keycloak.authentication.AuthenticationProcessor;
+
 import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.List;
+import java.util.Base64;
+
+import java.net.URI;
+
+import java.time.Instant;
 
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Cookie;
+import jakarta.ws.rs.core.NewCookie;
 
+import org.apache.commons.net.util.SubnetUtils;
+import org.apache.commons.net.util.SubnetUtils.SubnetInfo;
 import org.ietf.jgss.GSSContext;
 import org.ietf.jgss.GSSCredential;
 import org.ietf.jgss.GSSManager;
 import org.ietf.jgss.GSSName;
 import org.ietf.jgss.Oid;
 
+/**
+ * @author <a href="mailto:franciaa@uio.no">Francis Augusto Medeiros-Logeay</a>
+ * @version $Revision: 1 $
+ * Based on original code by <a href="https://github.com/slominskir/KeycloakConditionalSpnegoAuthenticator">KeycloakConditionalSpnegoAuthenticator</a>
+ */
 public class ConditionalKerberos extends SpnegoAuthenticator {
 
     private static Logger logger = Logger.getLogger(ConditionalKerberos.class);
@@ -28,59 +45,95 @@ public class ConditionalKerberos extends SpnegoAuthenticator {
 
     @Override
     public void authenticate(AuthenticationFlowContext context) {
-        AuthenticationSessionModel session = context.getAuthenticationSession();
-         HttpRequest request = context.getHttpRequest();
-        String authorizationHeader = request.getHttpHeaders().getRequestHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-        logger.info(authorizationHeader);
+      AuthenticatorConfigModel config = context.getAuthenticatorConfig();
+      String logout = config.getConfig().get("kerberos.logout");
+      Boolean allow_logout = true;
+      if ((logout == null) || (logout == "false")){
+          allow_logout = false;
+      } 
+      if (hasCookie(context) && allow_logout ){
+        context.attempted();
+        return;
+      }
 
-        String spnegoToken = "";
-        
-        if (authorizationHeader != null && authorizationHeader.startsWith("Negotiate ")) {
-          // Extract the SPNEGO token
-          spnegoToken = authorizationHeader.substring("Negotiate ".length());
-
-            if (!isKerberosTicket(context,spnegoToken)) {
-              logger.info("Skip Kerberos because of different domain.");
-            //  context.attempted();
-            //  return;
-            }
+      // Check if Kerberos authentication is allowed 
+      if (withinAllowedNetworks(context) && (( !hasCookie(context) && allow_logout) || (!allow_logout))) {
+            super.authenticate(context);
+            setCookieIfSuccessfull(context);
         } else {
-         //   logger.info("No kerberos ticket was sent.");
-          //  context.attempted();
-          //  return;
-        }
-        logger.info("Auttempting to authenticate with kerberos");
-        
-        super.authenticate(context);
+                 context.attempted();
+                 return;
+               }
+      if ((context.getStatus() == FlowStatus.SUCCESS) && allow_logout)
+      { 
+         setCookieIfSuccessfull(context); 
+      }
     }
 
-
-    public boolean isKerberosTicket(AuthenticationFlowContext context, String spnegoToken){
-      try {
+    public boolean withinAllowedNetworks(AuthenticationFlowContext context){
         AuthenticatorConfigModel config = context.getAuthenticatorConfig();
-        GSSManager gssManager = GSSManager.getInstance();
-        Oid krb5Oid = new Oid("1.2.840.113554.1.2.2");
-        GSSContext gssContext = gssManager.createContext((GSSCredential) null);
-        byte[] tokenBytes = java.util.Base64.getDecoder().decode(spnegoToken);
-        byte[] outToken = gssContext.acceptSecContext(tokenBytes, 0, tokenBytes.length);
-
-        if (!gssContext.isEstablished()) {
-            return false; // Authentication context could not be established
+        String networks = config.getConfig().get("kerberos.networks"); 
+        String ip_addr = context.getHttpRequest().getHttpHeaders().getRequestHeaders().getFirst("X-Forwarded-For");
+        boolean kerberos = false;
+        // check if user is allowed to use kerberos
+        if (networks != null){
+          String[] networks_list = networks.split("##"); 
+          for (String net : networks_list){
+            try 
+            {
+              SubnetInfo subnet = (new SubnetUtils(net)).getInfo();
+              if (subnet.isInRange(ip_addr)){
+                kerberos = true;
+                logger.debug("IP address within allowed ranges for Kerberos authentication: "+ip_addr); 
+                break;
+              }
+            }catch (Exception e){ logger.warn("Can't parse IP range kerberos allowed in configuration.");}
+          }
         }
-        GSSName clientName = gssContext.getSrcName();
-        String clientPrincipal = clientName.toString();
-        String kerberosPrincipal = config.getConfig().get("kerberos.domain"); 
-        logger.info("Client principal: "+clientPrincipal);
-        // Check if the client principal is from the expected domain
-        if (clientPrincipal.endsWith("@"+kerberosPrincipal)) {
-            return true; // Valid ticket from your domain
+        //check if the user isn't allowed to use kerberos
+        String exclude_networks = config.getConfig().get("kerberos.excluded_networks");
+        if (exclude_networks != null){
+          String[] exclude_list = exclude_networks.split("##");
+          for (String net : exclude_list){
+            try 
+            {
+              SubnetInfo subnet = (new SubnetUtils(net)).getInfo();
+              if (subnet.isInRange(ip_addr)){
+                kerberos = false;
+                logger.debug("IP address within networks excluded for kerberos authentication: "+ip_addr); 
+                break;
+              }
+            }catch (Exception e){ logger.warn("Can't parse IP range in exclude list configuration."); continue;  }
+          }
         }
-
-    } catch (Exception e) {
-        e.printStackTrace(); // Handle exceptions appropriately
-    }  
-
-       return false;
+        return kerberos;
     }
+
+    public boolean hasCookie(AuthenticationFlowContext context){
+        Cookie cookie = context.getHttpRequest().getHttpHeaders().getCookies().get("__Secure-SKIP_KERBEROS");
+        return  cookie != null;
+    }
+
+    private void setCookieIfSuccessfull(AuthenticationFlowContext context){
+    
+     AuthenticatorConfigModel config = context.getAuthenticatorConfig();
+     int maxCookieAge = 60 * 60 * 24 ; // 1 day
+     if (config != null && config.getConfig().get("cookie.age") != null) 
+        {
+          maxCookieAge = Integer.valueOf(config.getConfig().get("cookie.age"));
+        }
+      URI uri = context.getUriInfo().getBaseUriBuilder().path("realms").path(context.getRealm().getName()).build();
+      String date = Instant.now().plusSeconds(maxCookieAge).toString();
+      String message="true";
+
+      HttpResponse response = context.getSession().getContext().getHttpResponse();
+      NewCookie newCookie = new NewCookie.Builder("__Secure-SKIP_KERBEROS").value(message)
+                                .path(uri.getRawPath())
+                                .maxAge(maxCookieAge)
+                                .secure(true)
+                                .build();
+      response.setCookieIfAbsent(newCookie); 
+    }
+
 
 }
--- src/main/java/no/uio/keycloak/ConditionalKerberos/ConditionalKerberosFactory.java
+++ src/main/java/no/uio/keycloak/ConditionalKerberos/ConditionalKerberosFactory.java
@@ -17,7 +17,7 @@ import java.util.List;
 import static org.keycloak.provider.ProviderConfigProperty.STRING_TYPE;
 
 public class ConditionalKerberosFactory implements AuthenticatorFactory {
-    public static final String PROVIDER_ID = "conditional-auth-spnego";
+    public static final String PROVIDER_ID = "conditional-kerberos-login";
     static ConditionalKerberos SINGLETON = new ConditionalKerberos();
 
     @Override
@@ -81,13 +81,47 @@ public class ConditionalKerberosFactory implements AuthenticatorFactory {
     }
     private static final List<ProviderConfigProperty> configProperties = new ArrayList<ProviderConfigProperty>();
 
+   
     static {
         ProviderConfigProperty property;
         property = new ProviderConfigProperty();
-        property.setName("kerberos.domain");
-        property.setLabel("Kerberos domain");
+        property.setName("kerberos.logout");
+        property.setLabel("Skip kerberos on new login");
+        property.setType(ProviderConfigProperty.BOOLEAN_TYPE);
+        property.setHelpText("When this is set, a cookie is set after a successfull authentication. If the user logs out, the cookie will be checked when logging in to prevent kerberos authentication.");
+        configProperties.add(property);
+    }
+
+
+    static {
+        ProviderConfigProperty property;
+        property = new ProviderConfigProperty();
+        property.setName("cookie.age");
+        property.setLabel("Cookie age");
         property.setType(ProviderConfigProperty.STRING_TYPE);
-        property.setHelpText("The allowed realm for Kerberos authentication.");
+        property.setHelpText("When logging in with Kerberos, a cookie is saved to prevent a new automatic login with Kerberos. This allows for username/password login after logging out/.");
+        configProperties.add(property);
+    }
+
+
+    static {
+        ProviderConfigProperty property;
+        property = new ProviderConfigProperty();
+        property.setName("kerberos.networks");
+        property.setLabel("IP ranges for kerberos login");
+        property.setType(ProviderConfigProperty.MULTIVALUED_STRING_TYPE);
+        property.setHelpText("Only hosts within these IP ranges (in CIDR format) are allowed to login with Kerberos. If empty, no kerberos login will be allowed.");
+        configProperties.add(property);
+    }
+
+
+    static {
+        ProviderConfigProperty property;
+        property = new ProviderConfigProperty();
+        property.setName("kerberos.excluded_networks");
+        property.setLabel("IP ranges to exclude from Kerberos login");
+        property.setType(ProviderConfigProperty.MULTIVALUED_STRING_TYPE);
+        property.setHelpText("Exclude these networks from the networks above.");
         configProperties.add(property);
     }
 
